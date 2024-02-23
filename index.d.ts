cbbc30a592bb6cd9de6bb64bfbbe3fe4fc21aa01
Ryan Dsouza <r.dsouza@newmotion.com>
2024-02-23 18:18:43 +0000

    add types for aws-sdk v3 and lambda example

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,14 @@
 import { Connection, Transport } from '@opensearch-project/opensearch';
 import AWS from 'aws-sdk';
+import { AwsCredentialIdentity } from '@smithy/types';
 
 export type Connector = {
   Connection: typeof Connection;
   Transport: typeof Transport;
 };
 
-export const ConnectorFactory: (awsConfig: AWS.Config) => Connector;
+export const ConnectorFactory: (
+  awsConfig: AWS.Config | AwsCredentialIdentity
+) => Connector;
 
 export default ConnectorFactory;
